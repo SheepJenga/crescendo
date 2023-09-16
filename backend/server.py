ca0448d062655b3f2d66d091ddc83e4d3c93a8e3
Shepard Jiang <shepardjiang2025@gmail.com>
2023-09-16 15:34:42 -0400

    Implement basic endpoint and consumer example

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # This will enable CORS for all routes
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return "wtf"
+    return {'score': 1}
+
+@app.route("/testing")
+def test():
+    return {'testing': 123}
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port='8080')
+    app.run(port=8080)
