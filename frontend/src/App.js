71bf914e9c663bcf6a9c84b74413536e0f14cc6e
Tobe Obochi <tobe@mit.edu>
2023-09-16 16:15:00 -0400

    start frontend design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Header from './/components/header.js';
+import Home from './components/pages/homepage.js';
+import Feed from './components/pages/feed.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   const [data, setData] = useState([{}])
@@ -14,10 +19,19 @@ function App() {
 
   return (
     <div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/feed' component={Feed} />
+      </Routes>
+    </Router>
+    <div>
       <p>
         {data.score}
         {console.log(data)}
       </p>
+    </div>
     </div>
   );
 }
