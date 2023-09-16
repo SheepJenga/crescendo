c81bd9a7df9c584275f3e4a5d8e6d6bb58f82086
Shepard Jiang <shepardjiang2025@gmail.com>
2023-09-16 16:13:36 -0400

    Add initial data models

---
+++ backend/models/creator.py
@@ -0,0 +1,20 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+class Creator(DeclarativeBase):
+    __tablename__ = "creator"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    songs: List["Song"]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+    
+class Song():
+    pass
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <div>
       <p>
+        {data.score}
         {console.log(data)}
       </p>
     </div>
