0d3a829f02a5facc1262731e5662d35b02f5928f
Tobe Obochi <tobe@mit.edu>
2023-09-17 08:39:57 -0400

    we can pretend the login works fine

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { b64utoutf8, KJUR } from 'jsrsasign';
+import { useNavigate } from "react-router-dom";
 
 const onSuccess = (credentialResponse) => {
-    console.log(credentialResponse)
-    var sJWT = credentialResponse.credential
-    var headerObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(sJWT.split(".")[0]))
-    var payloadObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(sJWT.split(".")[1]))
-    
+    console.log(credentialResponse);
+    // var sJWT = credentialResponse.credential;
+    // var headerObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(sJWT.split(".")[0]));
+    // var payloadObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(sJWT.split(".")[1]));
 }
 
 const Login = (setUser) => {
+    const navigate = useNavigate();
     return (
         <GoogleOAuthProvider clientId="751367345812-ba8g7na7feqt60h1tfg56bv932hcil00.apps.googleusercontent.com">
             <GoogleLogin
-                onSuccess={onSuccess}
+                onSuccess={()=> {onSuccess();
+                    navigate("home");}}
                 onError={() => {
                     console.log('Login Failed:');
                 }}
