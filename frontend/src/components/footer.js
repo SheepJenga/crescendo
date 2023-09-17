4dfbf548d4297009836526a50becab30ee00977b
Tobe Obochi <tobe@mit.edu>
2023-09-17 06:18:04 -0400

    up to date ui

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <div>
+            <div class="App-footer">
+                <p>©2023 sheepjenga, inc. all rights reserved. if you're reading this it's too late.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
